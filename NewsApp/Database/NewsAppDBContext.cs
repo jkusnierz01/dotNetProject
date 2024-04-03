598e70b57a3f7fd5d15a44dc90e610af23bafa6e
jkusnierz01 <jedrek33@onet.pl>
2024-04-03 23:10:58 +0200

    dbconfig_part1-unfinished

---
+++ NewsApp/Database/NewsAppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Database
+{
+    public class NewsAppDBContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<UserCategory> UserCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
